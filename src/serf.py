793b739a1e0782edc8c86502bc714d358aa07137
rindyokra <rindyokra@gmail.com>
2024-01-28 13:49:42 -0500

    Less Dirty

--- src/serf.py
+++ src/serf.py
@@ -4,15 +4,15 @@ from typing import Any, Callable
 from src.util import date_check, gend, debug
 
 
-class C:
+class Intern:
     def __init__(self, user: Any) -> None:
         # Static
         self.user = user
 
         # Operations
         self.ctx = None
-        self.ama_flag = None
-        self.reap_flag = None
+        self.ama_mode = None
+        self.reap_mode = None
         self.start = None
         self.sun = None
         self.whisp_cache = None
@@ -21,16 +21,22 @@ class C:
         self.user_group = None
         self.target = None
 
-    def setup(self, ctx: Any, start: dt.date, collect_flags: str) -> None:
-        self.ctx = (ctx,)
+    def setup(
+        self, ctx: Any, collect_flags: str, start: dt.date, target: int | None
+    ) -> None:
+        self.ctx = ctx
         self.start = start
         self.sun = start
         self.whisp_cache = []
-        modes(collect_flags)
+        (a, r) = self.modes(collect_flags)
+        self.ama_mode = a
+        self.reap_mode = r
+
+        self.target = target
 
     def reset(self):
-        self.ama_flag = None
-        self.reap_flag = None
+        self.ama_mode = None
+        self.reap_mode = None
         self.start = None
         self.sun = None
         self.whisp_cache = None
@@ -51,26 +57,26 @@ class C:
         return breath[ing]
 
     def how_to_reap(self, ing: str) -> Callable:
-        def the_user() -> bool:
+        def some() -> bool:
             return len(self.whisp_cache) < self.target
 
-        def a_user() -> bool:
+        def alll() -> bool:
             return self.sun < dt.now()
 
-        def any_user() -> bool:
+        def day() -> bool:
             return date_check(self.start, self.whisp_cache)
 
-        slic: dict[str, Callable] = {"m": the_user, "g": a_user, "a": any_user}
+        slic: dict[str, Callable] = {"k": some, "n": alll, "d": day}
         return slic[ing]
 
-    def mode_flags(self, collect_flags: str) -> tuple[Callable, Callable]:
+    def modes(self, collect_flags: str) -> tuple[Callable, Callable]:
         sea: str = collect_flags[0]
         land: str = collect_flags[1]
         return (self.how_to_div(sea), self.how_to_reap(land))
 
     async def ama(self) -> list[Any]:
         whisps = await self.ctx.channel.history(
-            limit=200, oldest_first=True, after=sun
+            limit=200, oldest_first=True, after=self.sun
         ).flatten()
         self.sun = whisps[-1].created_at
         basket = [whisp for whisp in whisps if self.ama_mode(whisp)]
@@ -80,7 +86,7 @@ class C:
     async def reap(self) -> None:
         try:
             while self.reap_mode():
-                whisp = await ama()
+                whisp = await self.ama()
                 self.whisp_cache += whisp
                 print(f"\nCollecting from: {self.sun}")
                 print(f"Basket size: {len(self.whisp_cache)}")
@@ -89,12 +95,22 @@ class C:
 
 
 async def john_alite(whisp_cache: list[Any]):
-    print(f"\nI stawted at {dt.now()}\n\t-john_alite")
+    print(
+        f"""
+            I  stawted at {dt.now()}
+            \t-john_alite
+            """
+    )
     count = 0
     for whisp in whisp_cache:
         count += 1
         print(f"#{count}", end="", flush=True)
         time.sleep(2)
         await whisp.delete()
-    print(f"\nI  finished at {dt.now()}\n\t-john_alite")
+    print(
+        f"""\n
+            I  finished at {dt.now()}
+            \t-john_alite
+            """
+    )
     return
